11f47730c1eb1d69034002103770ec3b0a7e9c2b
ArosanSB <asb190401@gmail.com>
2024-04-18 22:22:23 +0200

    Add project files.

---
+++ NorthWind-UM/Program.cs
@@ -0,0 +1,12 @@
+﻿using NorthWind_UM.Models;
+
+
+
+NorthwindContext context = new NorthwindContext();
+
+
+foreach (Customer C in context.Customers)
+{
+    Console.WriteLine($"Name: {C.ContactName}");
+}
+Console.ReadLine();
